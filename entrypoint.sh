732fccb0f731d97bbdead31d5b898ecfafdff2df
Baptiste Langlade <Baptouuuu@users.noreply.github.com>
2020-03-24 18:41:08 +0100

    specify the new password to use for the neo4j user

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 docker_run="docker run"
-docker_run="$docker_run -d -p 7474:7474 neo4j:$INPUT_TAG"
+docker_run="$docker_run -d -p 7474:7474 --env=NEO4J_AUTH=neo4j/$INPUT_PASSWORD neo4j:$INPUT_TAG"
 
 sh -c "$docker_run"
